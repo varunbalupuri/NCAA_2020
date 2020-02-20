0b0a19a586a6b8d312954fa494f1277b0d44639d
Varun Balupuri <v.balupuri@signalocean.com>
2020-02-20 16:53:58 +0000

    incorporate seasonal box averages as feature

--- benchmark_model.py
+++ benchmark_model.py
@@ -8,123 +8,150 @@ from catboost import CatBoostClassifier, Pool, cv
 
 YEAR = 2019
 
+def generate_season_statistics(detailed_match_results, season):
+    teamids = list(set(detailed_match_results['LTeamID'].unique().tolist() + detailed_match_results['WTeamID'].unique().tolist()))
+    d = {}
+    for teamid in teamids:
+        stats = ['FGM', 'FGA', 'FGM3', 'FGA3', 'FTM', 'FTA', 'OR', 'DR', 'Ast', 'TO', 'Stl', 'Blk', 'PF']
 
-def create_train_test_dfs(results, kenpoms, massey_ordinals, seeds):
+        winning_games = detailed_match_results[(detailed_match_results['WTeamID'] == teamid) &
+                                               (detailed_match_results['Season'] == season)][
+                        [f'W{s}' for s in stats]]
+        winning_games.columns = [colname.lstrip('W') for colname in winning_games.columns]
+        losing_games = detailed_match_results[(detailed_match_results['LTeamID'] == teamid) &
+                                              (detailed_match_results['Season'] == season)][
+                        [f'L{s}' for s in stats]]
+        losing_games.columns = [colname.lstrip('L') for colname in losing_games.columns]
+        d[teamid] = pd.concat([winning_games, losing_games]).mean()
+    return pd.DataFrame(d).T
+
+
+def create_train_test_dfs(results, kenpoms, massey_ordinals, seeds, season_stats):
     l = []
     errors = []
+    mean_kenpoms = kenpoms[['AdjEM','AdjO','AdjD','AdjT','Luck','OppO','OppD']].mean()
     for _, row in results.iterrows():
-        # randomly pick team1 and team2
-        teams = [row['WTeamID'], row['LTeamID']]
-        team1 = np.random.choice(teams)
-        team2 = [i for i in teams if i != team1][0]
-        winner = row['WTeamID']
+        # repeat this entire thing 3 times
+        for iii in range(3):
+            # randomly pick team1 and team2
+            teams = [row['WTeamID'], row['LTeamID']]
+            team1 = np.random.choice(teams)
+            team2 = [i for i in teams if i != team1][0]
+            winner = row['WTeamID']
 
-        if winner == team1:
-            team1wins = True
-            team1score = row['WScore']
-            team2score = row['LScore']
-            if row['WLoc'] == 'H':
-                Team1Home = True
-                Team2Home = False
-            elif row['WLoc'] == 'A':
-                Team1Home = False
-                Team2Home = True
+            if winner == team1:
+                team1wins = True
+                team1score = row['WScore']
+                team2score = row['LScore']
+                if row['WLoc'] == 'H':
+                    Team1Home = True
+                    Team2Home = False
+                elif row['WLoc'] == 'A':
+                    Team1Home = False
+                    Team2Home = True
+                else:
+                    Team1Home = False
+                    Team2Home = False
+
+            if winner == team2:
+                team1wins = False
+                team2score = row['WScore']
+                team1score = row['LScore']
+                if row['WLoc'] == 'H':
+                    Team1Home = False
+                    Team2Home = True
+                elif row['WLoc'] == 'A':
+                    Team1Home = True
+                    Team2Home = False
+                else:
+                    Team1Home = False
+                    Team2Home = False
+
+            team1_kenpoms = kenpoms[kenpoms['TeamID'] == int(team1)]
+            if len(team1_kenpoms) == 0:
+                errors.append(team1)
+                team1_kenpoms = mean_kenpoms
             else:
-                Team1Home = False
-                Team2Home = False
+                team1_kenpoms = team1_kenpoms.iloc[0]
 
-        if winner == team2:
-            team1wins = False
-            team2score = row['WScore']
-            team1score = row['LScore']
-            if row['WLoc'] == 'H':
-                Team1Home = False
-                Team2Home = True
-            elif row['WLoc'] == 'A':
-                Team1Home = True
-                Team2Home = False
+            team2_kenpoms = kenpoms[kenpoms['TeamID'] == int(team2)]
+            if len(team2_kenpoms) == 0:
+                errors.append(team2)
+                team2_kenpoms = mean_kenpoms
             else:
-                Team1Home = False
-                Team2Home = False
+                team2_kenpoms = team2_kenpoms.iloc[0]
 
-        team1_kenpoms = kenpoms[kenpoms['TeamID'] == int(team1)]
-        if len(team1_kenpoms) == 0:
-            errors.append(team1)
-            team1_kenpoms = mean_kenpoms
-        else:
-            team1_kenpoms = team1_kenpoms.iloc[0]
+            Diff_AdjEM = team1_kenpoms['AdjEM'] - team2_kenpoms['AdjEM']
+            Diff_AdjO = team1_kenpoms['AdjO'] - team2_kenpoms['AdjO']
+            Diff_AdjD = team1_kenpoms['AdjD'] - team2_kenpoms['AdjD']
+            Diff_AdjT = team1_kenpoms['AdjT'] - team2_kenpoms['AdjT']
+            Diff_Luck = team1_kenpoms['Luck'] - team2_kenpoms['Luck']
+            Diff_OppO = team1_kenpoms['OppO'] - team2_kenpoms['OppO']
+            Diff_OppD = team1_kenpoms['OppD'] - team2_kenpoms['OppD']
 
-        team2_kenpoms = kenpoms[kenpoms['TeamID'] == int(team2)]
-        if len(team2_kenpoms) == 0:
-            errors.append(team2)
-            team2_kenpoms = mean_kenpoms
-        else:
-            team2_kenpoms = team2_kenpoms.iloc[0]
 
-        Diff_AdjEM = team1_kenpoms['AdjEM'] - team2_kenpoms['AdjEM']
-        Diff_AdjO = team1_kenpoms['AdjO'] - team2_kenpoms['AdjO']
-        Diff_AdjD = team1_kenpoms['AdjD'] - team2_kenpoms['AdjD']
-        Diff_AdjT = team1_kenpoms['AdjT'] - team2_kenpoms['AdjT']
-        Diff_Luck = team1_kenpoms['Luck'] - team2_kenpoms['Luck']
-        Diff_OppO = team1_kenpoms['OppO'] - team2_kenpoms['OppO']
-        Diff_OppD = team1_kenpoms['OppD'] - team2_kenpoms['OppD']
+            # box season averages
+            t1_stats = season_stats.loc[team1]
+            t2_stats = season_stats.loc[team2]
 
-        # massey ordinals
-        try:
-            team1_massey = massey_ordinals[
-                (massey_ordinals['RankingDayNum'] < row['DayNum']) &
-                (massey_ordinals['TeamID'] == team1)
-                ].groupby('RankingDayNum').mean().idxmax()['OrdinalRank']
-        except Exception:
-            team1_massey = \
-            massey_ordinals[(massey_ordinals['TeamID'] == team1)].groupby('RankingDayNum').mean().iloc[0]['OrdinalRank']
+            diff_stats = t1_stats - t2_stats
 
-        try:
-            team2_massey = massey_ordinals[
-                (massey_ordinals['RankingDayNum'] < row['DayNum']) &
-                (massey_ordinals['TeamID'] == team2)
-                ].groupby('RankingDayNum').mean().idxmax()['OrdinalRank']
+            # massey ordinals
+            try:
+                team1_massey = massey_ordinals[
+                    (massey_ordinals['RankingDayNum'] < row['DayNum']) &
+                    (massey_ordinals['TeamID'] == team1)
+                    ].groupby('RankingDayNum').mean().idxmax()['OrdinalRank']
+            except Exception:
+                team1_massey = \
+                massey_ordinals[(massey_ordinals['TeamID'] == team1)].groupby('RankingDayNum').mean().iloc[0]['OrdinalRank']
 
-        except Exception:
-            team2_massey = \
-            massey_ordinals[(massey_ordinals['TeamID'] == team2)].groupby('RankingDayNum').mean().iloc[0]['OrdinalRank']
+            try:
+                team2_massey = massey_ordinals[
+                    (massey_ordinals['RankingDayNum'] < row['DayNum']) &
+                    (massey_ordinals['TeamID'] == team2)
+                    ].groupby('RankingDayNum').mean().idxmax()['OrdinalRank']
 
-        Diff_MasseyOrdinal = team1_massey - team2_massey
+            except Exception:
+                team2_massey = \
+                massey_ordinals[(massey_ordinals['TeamID'] == team2)].groupby('RankingDayNum').mean().iloc[0]['OrdinalRank']
 
-        try:
-            team1_seed = seeds[seeds['TeamID'] == team1].iloc[0]['seednumber']
-        except:
-            team1_seed = 16
-        try:
-            team2_seed = seeds[seeds['TeamID'] == team2].iloc[0]['seednumber']
-        except:
-            team2_seed = 16
+            Diff_MasseyOrdinal = team1_massey - team2_massey
 
-        Diff_seeds = team1_seed - team2_seed
+            try:
+                team1_seed = seeds[seeds['TeamID'] == team1].iloc[0]['seednumber']
+            except:
+                team1_seed = 16
+            try:
+                team2_seed = seeds[seeds['TeamID'] == team2].iloc[0]['seednumber']
+            except:
+                team2_seed = 16
 
-        row = {
-            # 'Team1ID': team1,
-            # 'Team2ID': team2,
-            'Team1Home': Team1Home,
-            'Team2Home': Team2Home,
-            # 'Team1Score': team1score,   #obviously cannot use score to predict game!
-            # 'Team2Score': team2score,
-            'Team1Wins': team1wins,
+            Diff_seeds = team1_seed - team2_seed
 
-            'Diff_AdjEM': Diff_AdjEM,
-            'Diff_AdjO': Diff_AdjO,
-            'Diff_AdjD': Diff_AdjD,
-            'Diff_AdjT': Diff_AdjT,
-            # 'Diff_Luck':Diff_Luck,
-            'Diff_OppO': Diff_OppO,
-            'Diff_OppD': Diff_OppD,
+            new_row = {
+                # 'Team1ID': team1,
+                # 'Team2ID': team2,
+                'Team1Home': Team1Home,
+                'Team2Home': Team2Home,
+                # 'Team1Score': team1score,   #obviously cannot use score to predict game!
+                # 'Team2Score': team2score,
+                'Team1Wins': team1wins,
 
-            'Diff_MasseyOrdinal': Diff_MasseyOrdinal,
-            'Diff_seeds': Diff_seeds
+                'Diff_AdjEM': Diff_AdjEM,
+                'Diff_AdjO': Diff_AdjO,
+                'Diff_AdjD': Diff_AdjD,
+                'Diff_AdjT': Diff_AdjT,
+                'Diff_Luck': Diff_Luck,
+                'Diff_OppO': Diff_OppO,
+                'Diff_OppD': Diff_OppD,
 
-        }
+                'Diff_MasseyOrdinal': Diff_MasseyOrdinal,
+                'Diff_seeds': Diff_seeds
 
-        l.append(row)
+            }
+            new_row.update(diff_stats)
+            l.append(new_row)
     errors = set(errors)
     print(f'error rows = {len(errors)}: {errors}')
     df = pd.DataFrame.from_records(l)
@@ -136,11 +163,13 @@ def create_train_test_dfs(results, kenpoms, massey_ordinals, seeds):
 
 
 def train(year=2019, kenpom_lag=False, all_years=False):
-    NCAA_tournament_results = pd.read_csv('data/mens/MNCAATourneyCompactResults.csv')
+    NCAA_tournament_results = pd.read_csv('data/mens/MNCAATourneyDetailedResults.csv')
     NCAA_tournament_results = NCAA_tournament_results[NCAA_tournament_results['Season'] == year]
 
-    NCAA_season_results = pd.read_csv('data/mens/MRegularSeasonCompactResults.csv')
+    NCAA_season_results = pd.read_csv('data/mens/MRegularSeasonDetailedResults.csv')
     NCAA_season_results = NCAA_season_results[NCAA_season_results['Season'] == year]
+
+    season_stats = generate_season_statistics(NCAA_season_results, year)
 
     # kenpoms
     if kenpom_lag:
@@ -160,8 +189,8 @@ def train(year=2019, kenpom_lag=False, all_years=False):
     seeds['seednumber'] = seeds['Seed'].apply(lambda x: int(''.join([i for i in x if i.isdigit()])))
     seeds['region'] = seeds['Seed'].apply(lambda x: x[0])
 
-    X_train, y_train = create_train_test_dfs(NCAA_season_results, kenpoms, massey_ordinals, seeds)
-    X_test, y_test = create_train_test_dfs(NCAA_tournament_results, kenpoms, massey_ordinals, seeds)
+    X_train, y_train = create_train_test_dfs(NCAA_season_results, kenpoms, massey_ordinals, seeds, season_stats)
+    X_test, y_test = create_train_test_dfs(NCAA_tournament_results, kenpoms, massey_ordinals, seeds, season_stats)
 
     categorical_idxs = [0, 1]
     model = CatBoostClassifier(custom_loss=['Logloss'], logging_level='Verbose')
@@ -179,17 +208,20 @@ def train(year=2019, kenpom_lag=False, all_years=False):
 
     return model
 
+
 def train_giant_model():
     X_trains = []
     y_trains = []
     X_tests = []
     y_tests = []
-    for year in range(2010, 2020):
-        NCAA_tournament_results = pd.read_csv('data/mens/MNCAATourneyCompactResults.csv')
+    for year in range(2010, 2018):
+        NCAA_tournament_results = pd.read_csv('data/mens/MNCAATourneyDetailedResults.csv')
         NCAA_tournament_results = NCAA_tournament_results[NCAA_tournament_results['Season'] == year]
 
-        NCAA_season_results = pd.read_csv('data/mens/MRegularSeasonCompactResults.csv')
+        NCAA_season_results = pd.read_csv('data/mens/MRegularSeasonDetailedResults.csv')
         NCAA_season_results = NCAA_season_results[NCAA_season_results['Season'] == year]
+
+        season_stats = generate_season_statistics(NCAA_season_results, year)
 
         # # kenpoms
         # if kenpom_lag:
@@ -209,8 +241,8 @@ def train_giant_model():
         seeds['seednumber'] = seeds['Seed'].apply(lambda x: int(''.join([i for i in x if i.isdigit()])))
         seeds['region'] = seeds['Seed'].apply(lambda x: x[0])
 
-        X_train, y_train = create_train_test_dfs(NCAA_season_results, kenpoms, massey_ordinals, seeds)
-        X_test, y_test = create_train_test_dfs(NCAA_tournament_results, kenpoms, massey_ordinals, seeds)
+        X_train, y_train = create_train_test_dfs(NCAA_season_results, kenpoms, massey_ordinals, seeds, season_stats)
+        X_test, y_test = create_train_test_dfs(NCAA_tournament_results, kenpoms, massey_ordinals, seeds, season_stats)
 
         X_trains.append(X_train)
         y_trains.append(y_train)
@@ -232,4 +264,3 @@ if __name__ == '__main__':
 
     args = parser.parse_args()
 
-    if args.all_years:
